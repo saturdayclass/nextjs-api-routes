1505526e65d568e93ab74e8839f77eb8abfd3a31
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-04-09 16:29:17 +0700

    update: storeing data from form to dummy file

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+
 const handler = (req, res) => {
-  return res.status(200).json({
-    message: "It's Work"
-  })
-}
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const text = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(200).json({
+      message: 'Successfully added data!',
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({
+      message: "It's Work",
+    });
+  }
+};
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
 import { useRef } from 'react';
 function HomePage() {
-  
   const emailInput = useRef();
   const feedbackInput = useRef();
 
-  const submitForm =  (event) => {
+  const submitForm = async (event) => {
     event.preventDefault();
 
-    const enteredEmail  = emailInput.current.value;
-    const enteredFeedback  = feedbackInput.current.value;
-  }
+    const enteredEmail = emailInput.current.value;
+    const enteredFeedback = feedbackInput.current.value;
+
+    const dataForm = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    const req = await fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(dataForm),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const res = await req.json();
+    console.log(res);
+  };
 
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitForm}>
         <div>
-          <label for="email">Email Address</label>
+          <label htmlFor="email">Email Address</label>
           <input type="email" ref={emailInput} id="email" />
         </div>
         <div>
-          <label for="feedback">Your Feedback</label>
+          <label htmlFor="feedback">Your Feedback</label>
           <textarea type="text" ref={feedbackInput} id="feedback" rows="5" />
         </div>
         <button>Submit</button>
